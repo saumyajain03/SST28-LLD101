d042b2dcf795d7b23d01b1e4f56bc0c35b560d81
Saumya Jain <saumyyaaa.03@gmail.com>
2026-03-09 10:56:34 +0530

    completed flyweight assignment

--- flyweight-markers/src/com/example/map/MarkerStyleFactory.java
+++ flyweight-markers/src/com/example/map/MarkerStyleFactory.java
@@ -20,7 +20,14 @@ public class MarkerStyleFactory {
     public MarkerStyle get(String shape, String color, int size, boolean filled) {
         String key = shape + "|" + color + "|" + size + "|" + (filled ? "F" : "O");
         // TODO: return cached instance if present; otherwise create, cache, and return.
-        return new MarkerStyle(shape, color, size, filled);
+
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+        MarkerStyle markerStyle =new MarkerStyle(shape, color, size, filled);
+        cache.put(key, markerStyle);
+
+        return markerStyle;
     }
 
     public int cacheSize() {
